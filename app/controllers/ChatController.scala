27350dc25b89c5c06fbfe2a36553dcdfd3df2ecf
Göran Schumacher <schumacher@me.com>
2016-03-17 19:44:15 +0100

    Added index page that asks for chat name.

--- app/controllers/ChatController.scala
+++ app/controllers/ChatController.scala
@@ -1,15 +1,33 @@
 package controllers
 
+import models.User
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc.{Action, Controller}
+import javax.inject.Inject
+import play.api.i18n.I18nSupport
+import play.api.i18n.MessagesApi
 
 /**
  * @author Gøran Schumacher (GS) / Schumacher Consulting Aps
  * @version $Revision$ 17/03/2016
  */
-class ChatController extends Controller {
+class ChatController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  val userForm = Form(
+    mapping(
+      "name" -> text
+    )(User.apply)(User.unapply)
+  )
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(userForm))
+  }
+
+  val userPost = Action(parse.form(userForm)) { implicit request =>
+    val userData = request.body
+    val newUser = User(userData.name)
+    Redirect(routes.ChatController.openChat(newUser.name))
   }
 
   def openChat(name : String) = Action { implicit request =>
---
+++ app/models/User.scala
@@ -0,0 +1,7 @@
+package models
+
+/**
+ * @author Gøran Schumacher (GS) / Schumacher Consulting Aps
+ * @version $Revision$ 17/03/2016
+ */
+case class User(name: String)
